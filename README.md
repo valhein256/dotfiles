aaec4caec11f10d086a83366a6d3853f10ebaeb4
valhein <valhein256@gmail.com>
2020-07-30 12:42:28 +0800

    Add TODO: The installation steps of theme and font. And Add installation info to scripts/install_brew.sh.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Pre-processing
+    * Install docker
     * Install powerline fonts: https://github.com/powerline/fonts
     * Run ./install_brew.sh & ./install_brew_packages.py
     * Make sure you have pip3.
@@ -17,8 +18,8 @@
       * git config --global user.email ""
 
     * set local:
-      * git config --global user.name ""
-      * git config --global user.email ""
+      * git config --local user.name ""
+      * git config --local user.email ""
 
 # Vim setting
 Ref: 
@@ -27,4 +28,9 @@ Ref:
 # Error case
 Ref:
     1. https://blog.csdn.net/qq_42672770/article/details/87182133
-
+    2. https://stackoverflow.com/questions/13762280/zsh-compinit-insecure-directories/41674919#41674919
+     
+# Todo
+    1. The installation steps of theme and font
+      a. https://github.com/ryanoasis/nerd-fonts#option-1-download-and-install-manually 
+      b. https://github.com/mbadolato/iTerm2-Color-Schemes
--- scripts/install_brew.sh
+++ scripts/install_brew.sh
@@ -1,5 +1,37 @@
 #!/bin/bash -e
 
+
+# ==> This script will install:
+# /usr/local/bin/brew
+# /usr/local/share/doc/homebrew
+# /usr/local/share/man/man1/brew.1
+# /usr/local/share/zsh/site-functions/_brew
+# /usr/local/etc/bash_completion.d/brew
+# /usr/local/Homebrew
+# ==> The following existing directories will be made group writable:
+# /usr/local/lib
+# ==> The following existing directories will have their owner set to wu-hsuan_lin:
+# /usr/local/lib
+# ==> The following existing directories will have their group set to admin:
+# /usr/local/lib
+# ==> The following new directories will be created:
+# /usr/local/bin
+# /usr/local/etc
+# /usr/local/include
+# /usr/local/sbin
+# /usr/local/share
+# /usr/local/var
+# /usr/local/opt
+# /usr/local/share/zsh
+# /usr/local/share/zsh/site-functions
+# /usr/local/var/homebrew
+# /usr/local/var/homebrew/linked
+# /usr/local/Cellar
+# /usr/local/Caskroom
+# /usr/local/Homebrew
+# /usr/local/Frameworks
+
+
 info () {
   printf "\r  [ \033[00;34m..\033[0m ] $1\n"
 }
