47be7ce216ac22c159cdf05eb810f4df50651e8a
valhein <valhein256@gmail.com>
2021-04-26 17:40:31 +0800

    Remove non-used scripts: scripts/brew_update_all.py

--- scripts/brew_update_all.py
+++
@@ -1,6 +0,0 @@
-#!/usr/local/bin/python
-
-import subprocess
-subprocess.call("brew update", shell=True)
-map(lambda x:subprocess.call("brew upgrade " + x, shell=True),
-    subprocess.check_output('brew outdated', shell=True).split())
