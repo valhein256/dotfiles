2ee610f31209f1dd9811c53ff13c6a7c04e8f907
lwh <valhein256@gmail.com>
2025-09-17 11:09:30 +0800

    Update

--- scripts/installations/packages.py
+++ scripts/installations/packages.py
@@ -8,23 +8,6 @@ BREW_TAP_LIST = """
 hashicorp/tap
 """
 
-# BREW_LIST_WITH_ARGS = """
-# vim --with-lua --with-luajit
-# macim --with-lua --with-luajit
-# findutils --with-default-names
-# gnu-indent --with-default-names
-# gnu-sed --with-default-names
-# gnu-tar --with-default-names
-# gnu-which --with-default-names
-# grep --with-default-names
-# wdiff --with-gettext
-# """
-
-# BREW_LIST = """ brew-cask binutils diffutils gawk gnutls gzip screen watch
-# wget zsh unrar cgdb make vim p7zip gnu-time gnu-which gnuplot grep sqlite tig
-# doxygen graphviz ctags cscope git gnu-tar tmux screen cmake
-# """
-
 BREW_LIST = """
     zsh git tree ag xquartz fzf sshs
     neovim ctags-exuberant
@@ -34,27 +17,17 @@ BREW_LIST = """
     awscli google-cloud-sdk amazon-q
 """
 
-# PIP_PACKAGE_LIST = """cython ipython pylint pep8 pyscope flask nikola markdown nose"""
-
 PIP_PACKAGE_LIST = """
     virtualenv
     poetry
 """
 
-# CABAL_PACKAGE_LIST = """happy pandoc hakyll ghc-mod hlint"""
-
-# SETTING_COMMANDS_INIT = """brew update cabal update"""
+NPM_PACKAGE_LIST = """
+    aws-cdk
+"""
 
 PREPARING = """rm -rf ~/.nvm && mkdir ~/.nvm"""
 SETTING_COMMANDS_INIT = """brew update"""
-
-# SETTING_COMMANDS_POST = """sudo rm -rf /usr/bin/tar
-# sudo ln -s /usr/local/bin/gtar /usr/bin/tar
-# sudo rm -rf /usr/bin/ctags
-# sudo ln -s /usr/local/bin/ctags /usr/bin/ctags
-# brew cask alfred link
-# brew linkapps"""
-
 
 def generateCommandByWords(cmd, words):
     packages = [p for p in words.split() if p]
@@ -73,9 +46,7 @@ def main():
         {"cmd": "brew tap", "list": BREW_TAP_LIST, "generator": generateCommandByWords},
         {"cmd": "brew install", "list": BREW_LIST, "generator": generateCommandByWords},
         {"cmd": "pipx install", "list": PIP_PACKAGE_LIST, "generator": generateCommandByWords},
-        # {"cmd": "brew install", "list": BREW_LIST_WITH_ARGS, "generator": generateCommandByLines},
-        # {"cmd": "cabal install", "list": CABAL_PACKAGE_LIST, "generator": generateCommandByWords},
-        # {"cmd": "", "list": SETTING_COMMANDS_POST, "generator": generateCommandByLines}
+        {"cmd": "npm install -g", "list": NPM_PACKAGE_LIST, "generator": generateCommandByWords},
     ]
 
     cmds = [c['generator'](c['cmd'], c['list']) for c in cmd_seeting]
